0d36bc1997bfd4a47421ad86ae282c7b318699f0
Your Name <you@example.com>
2023-10-28 11:32:50 +0300

    Filters

---
+++ src/app/filt/filt.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filt',
+  templateUrl: './filt.component.html',
+  styleUrls: ['./filt.component.scss'],
+})
+export class FiltComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

7786f3d939264a2ca8df844546c0b34cb92e619a
Piastr <viktor_kuzmin_98@mail.ru>
2023-12-16 00:01:17 +0300

    added logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,39 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
+
 export class AppComponent {
   public appPages = [
-    { title: 'Входящие', url: '/folder/inbox', icon: 'входящие' },
-    { title: 'Исходящие', url: '/folder/outbox', icon: 'исходящие' },
-    { title: 'Понравилось', url: '/folder/favorites', icon: 'понравившиеся' },
-    { title: 'Архив', url: '/folder/archived', icon: 'архив' },
-    { title: 'Корзина', url: '/folder/trash', icon: 'корзина' },
-    { title: 'Спам', url: '/folder/spam', icon: 'предупреждения/спам' },
-    { title: 'Filters', url:'/forms/filters',icon: 'reader-outline'},
-    { title: 'Validations', url: '/validations', icon: '' },  
+    { title: '1', url: '/folder/inbox', icon: 'входящие' },
+    { title: '2', url: '/folder/outbox', icon: 'исходящие' },
+    { title: '3', url: '/folder/favorites', icon: 'понравившиеся' },
+    { title: '4', url: '/folder/archived', icon: 'архив' },
+    { title: '5', url: '/folder/trash', icon: 'корзина' },
+    { title: '6', url: '/folder/spam', icon: 'предупреждения/спам' },
+    // Добавляем поле для перехода на страницу пометок
+    { title: '7', url: '/notes', icon: 'Пометки' },
+    { title: '8', url:'/filters',icon: 'reader-outline'},
+    { title: '9', url: '/validations', icon: '' },  
+    { title: '10', url: '/privacy-policy', icon: 'battery-full-outline' },
+    { title: '11', url: '/login', icon: '' },
   ];
   public labels = ['Семья', 'Друзья', 'Заметки', 'Работа', 'Путешествия', 'Напоминания'];
-  constructor() {}
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('ru');
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,47 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import {FormsComponent} from './forms/forms.component'
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+import { IonicRouteStrategy } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsComponent } from './forms/forms.component';
 import { ContactCardComponent } from './contact-card/contact-card.component';
-import {SettingsComponent} from "./settings/settings.component";
+import { SettingsComponent } from './settings/settings.component';
+import { SignupComponent } from './signup/signup.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { NotesComponent } from './notes/notes.component';
+
+// Фабрика для создания TranslateLoader с использованием HttpClient
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18s/', '.json');
+}
 
 @NgModule({
-  declarations: [AppComponent,FormsComponent, ContactCardComponent, SettingsComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  declarations: [AppComponent, FormsComponent, ContactCardComponent, SettingsComponent, SignupComponent, PrivacyPolicyComponent, NotesComponent, NotesComponent,],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    IonicModule.forRoot(), // Важно для доступности Ionic компонентов
+    AppRoutingModule,
+    HttpClientModule,
+    
+    TranslateModule.forRoot({ // Настройка ngx-translate
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    // Другие сервисы и провайдеры
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}

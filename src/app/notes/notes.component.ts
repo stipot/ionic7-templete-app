8655b3e732a2bba0319a5a76084329d132b90ca5
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-01-19 17:20:32 +0300

    feat: remove and update methods for service

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -12,21 +12,29 @@ export class NotesComponent implements OnInit {
 
   constructor(private userService: UserService) {
     this.userService.getData('notes').subscribe((res) => {
-      console.log(this.notes)
       this.notes.length = 0;
       res.map((element: any) => {
         this.notes.push(element);
       });
-      console.log(this.notes);
     });
   }
 
   saveData() {
     if (this.noteData) {
-      this.userService.addData(this.noteData, 'notes');
+      this.userService.addData('notes', this.noteData);
       this.noteData = '';
     }
-    console.log(this.noteData);
+  }
+  // Для изменения поля нужно ввести в главный инпут новые данные и нажать кнопку edit
+  updateDocument(id: string) {
+    if (this.noteData) {
+      this.userService.updateDocument('notes', id, this.noteData);
+      this.noteData = '';
+    }
+  }
+
+  removeDocument(id: string) {
+    this.userService.removeDocument('notes', id);
   }
 
   ngOnInit() {}
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { addDoc } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import {
+  addDoc,
+  deleteDoc,
+  doc,
+  getFirestore,
+  updateDoc,
+} from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private firestore: Firestore) {}
+  app = initializeApp(environment.firebase);
+  db = getFirestore(this.app);
+
   /**
    * Get data from Firestore
    * @param collectionName - component name
@@ -15,14 +26,46 @@ export class UserService {
     const collectionRef = collection(this.firestore, collectionName);
     return collectionData(collectionRef);
   }
-  
+
   /**
-   * Data store in Firestore
+   * Create document
    * @param data - json data
    * @param collectionName - component name
    */
-  addData(data: any, collectionName: any) {
+
+  async addData(collectionName: any, data: any) {
     const collectionRef = collection(this.firestore, collectionName);
-    addDoc(collectionRef, { data: data });
+    await addDoc(collectionRef, { data: data }).then(
+      // После создания документа с полем data мы получаем его id в базе данных и добавляем новое поле id при помощи функции updateDoc
+      (DocumentReference: any) => {
+        const collectionRef = doc(
+          this.db,
+          collectionName,
+          DocumentReference.id
+        );
+        updateDoc(collectionRef, { data, id: DocumentReference.id });
+      }
+    );
+  }
+
+  /**
+   * Update document's field
+   * @param collectionName - component name
+   * @param id - document's id
+   * @param data - json data
+   */
+  async updateDocument(collectionName: any, id: any, data: any) {
+    const collectionRef = doc(this.db, collectionName, id);
+    await updateDoc(collectionRef, { data: data });
+  }
+
+  /**
+   * Remove document
+   * @param collectionName - component name
+   * @param id - document's id
+   */
+  async removeDocument(collectionName: any, id: any) {
+    const collectionRef = doc(this.db, collectionName, id);
+    await deleteDoc(collectionRef);
   }
 }

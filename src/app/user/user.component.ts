5e6c99b3bc44264c82c1d7eacf1ac4372be51ce2
visyachok45 <bochkovadiana@yandex.ru>
2024-01-20 13:35:17 +0300

    Добавление

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,5 +10,19 @@ export class UserComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+  userProfile = {
+    "userImage": "src/assets/sample-images/notifications/diana.jpg",
+    "name": "Бочкова Диана Ашотовна",
+    "membership": "ПРО",
+    "job": "Программист-консультант, внедренец",
+    "likes": "1448",
+    "followers": "666K",
+    "following": "666",
+    "about": "Всем привет! Меня зовут Дианчик, я люблю работать и люблю всех вас!",
+    "friends": "Друзья",
+    "frnd1": "Майкл",
+    "frnd2": "Тревор",
+    "frnd3": "Франклин"
+}
 
 }

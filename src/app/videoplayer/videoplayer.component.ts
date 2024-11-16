a702575de00d3d13ce6f82302a46fe529e3f3115
khr1v <lalka888999@gmail.com>
2024-11-16 15:23:26 +0300

    feature: videoplayer

--- src/app/videoplayer/videoplayer.component.ts
+++ src/app/videoplayer/videoplayer.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'videoplayer',
+  selector: 'app-video-player',
   templateUrl: './videoplayer.component.html',
   styleUrls: ['./videoplayer.component.scss']
 })
 export class VideoPlayerComponent implements OnInit {
 
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggleVideo() {
+    const video = this.videoPlayer.nativeElement;
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
   }
 
+  stopVideo() {
+    const video = this.videoPlayer.nativeElement;
+    video.pause();
+    video.currentTime = 0;
+  }
+
+  setVolume(volume: number) {
+    const video = this.videoPlayer.nativeElement;
+    video.volume = volume;
+  }
 }

3c137eb8b941336498a071a9b517bce7bfe14e02
Your Name <you@example.com>
2024-12-14 17:38:03 +0300

    Add buttons with function controll music player

--- src/app/mplayer/mplayer.component.ts
+++ src/app/mplayer/mplayer.component.ts
@@ -7,14 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MplayerComponent  implements OnInit {
   public progress = 0;
+  public player_status = 'play';
 
   constructor() {
     setInterval(() => {
-      this.progress += 0.001;
-      if (this.progress > 1) {
-        this.progress = 0;
+      if (this.player_status == 'pause') {
+        this.progress += 0.001;
+        if (this.progress > 1) {
+          this.set_zero_bar();
+        }
+      } else {
+
       }
     }, 100);
+  }
+
+  set_zero_bar() {
+    this.progress = 0;
+  }
+
+  change_status() {
+    if (this.player_status == 'pause') {
+      this.player_status = 'play';
+    } else {
+      this.player_status = 'pause';
+    }
+  }
+
+  next_track() {
+    this.set_zero_bar();
+  }
+
+  prev_track() {
+    this.set_zero_bar();
   }
 
   ngOnInit() {}

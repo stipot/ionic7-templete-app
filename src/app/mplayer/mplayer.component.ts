6ca7c569e0b583e2e726857c3776f9a046ceb8fb
Your Name <you@example.com>
2024-12-14 16:39:33 +0300

    Add ion-card with progress bar

--- src/app/mplayer/mplayer.component.ts
+++ src/app/mplayer/mplayer.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mplayer.component.scss'],
 })
 export class MplayerComponent  implements OnInit {
+  public progress = 0;
 
-  constructor() { }
+  constructor() {
+    setInterval(() => {
+      this.progress += 0.001;
+      if (this.progress > 1) {
+        this.progress = 0;
+      }
+    }, 100);
+  }
 
   ngOnInit() {}
 

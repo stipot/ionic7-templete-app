805bcf0104cdf560772d512f21428e946fbd6c14
nastyatretyakova11 <nastyatretiakova11@mail.ru>
2025-04-05 18:29:02 +0300

    first проблема с bigint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import {MplayerComponent} from "./mplayer/mplayer.component";
 import { FileViewerComponent } from './file-viewer/file-viewer.component';
 import { MoodCalendarComponent } from './mood-calendar/mood-calendar.component';
 import { DragAndDropComponent } from './drag-and-drop/drag-and-drop.component';
+import { RsaComponent } from './rsa/rsa.component';
 
 // Фабрика для загрузчика переводов
 export function HttpLoaderFactory(http: HttpClient) {
@@ -73,7 +74,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     MplayerComponent,
     CameraComponent,
     FileViewerComponent,
-    DragAndDropComponent
+    DragAndDropComponent,
+    RsaComponent
   ],
   imports: [
     BrowserModule,

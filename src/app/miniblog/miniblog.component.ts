586c948a66b126203ccda7fc89c8b5699dc9eabf
student <student>
2025-05-03 13:10:45 +0300

    feat: add interface

--- src/app/miniblog/miniblog.component.ts
+++ src/app/miniblog/miniblog.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Blog {
+  name: string
+  chapter: string
+  src: string
+  text: string
+}
+
 @Component({
   selector: 'app-miniblog',
   templateUrl: './miniblog.component.html',
@@ -8,7 +15,9 @@ import { Component, OnInit } from '@angular/core';
 export class MiniblogComponent  implements OnInit {
 
   constructor() { }
-
+  blogs: Blog[] =[
+    {name: "Обзор Анаксы", chapter: "Игры", src: "https://www.goha.ru/s/A:CO/aO/xY8wmWZwYM.jpg", text: "В Honkai: Star Rail недавно вышел новый персонаж пути ветряной эрудиции. В этой статье расскажу о своих впечатлениях об этом персонаже."}
+  ]
   ngOnInit() {}
 
 }

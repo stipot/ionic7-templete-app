8cbc8af7c3ad92d3e58f0e9ed1e4f6e57861dbac
Klara <stydy000coffee@gmail.com>
2025-06-14 08:58:50 +0300

    feat: связания модуля к основному

--- src/app/miniblog/miniblog.component.ts
+++ src/app/miniblog/miniblog.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AddBlogModalComponent } from '../miniblog/add-blog-modal.component';
 
 interface Blog {
-  name: string
-  chapter: string
-  src: string
-  text: string
+  name: string;
+  chapter: string;
+  src: string;
+  text: string;
 }
 
 @Component({
@@ -12,12 +14,30 @@ interface Blog {
   templateUrl: './miniblog.component.html',
   styleUrls: ['./miniblog.component.scss'],
 })
-export class MiniblogComponent  implements OnInit {
+export class MiniblogComponent implements OnInit {
+  blogs: Blog[] = [
+    {
+      name: "Обзор Анаксы",
+      chapter: "Игры",
+      src: "https://www.goha.ru/s/A:CO/aO/xY8wmWZwYM.jpg",
+      text: "В Honkai: Star Rail недавно вышел новый персонаж пути ветряной эрудиции. В этой статье расскажу о своих впечатлениях об этом персонаже."
+    }
+  ];
 
-  constructor() { }
-  blogs: Blog[] =[
-    {name: "Обзор Анаксы", chapter: "Игры", src: "https://www.goha.ru/s/A:CO/aO/xY8wmWZwYM.jpg", text: "В Honkai: Star Rail недавно вышел новый персонаж пути ветряной эрудиции. В этой статье расскажу о своих впечатлениях об этом персонаже."}
-  ]
+  constructor(private modalCtrl: ModalController) {}
+
   ngOnInit() {}
 
+  async openModal() {
+    const modal = await this.modalCtrl.create({
+      component: AddBlogModalComponent
+    });
+
+    await modal.present();
+
+    const { data, role } = await modal.onDidDismiss();
+    if (role === 'confirm' && data) {
+      this.blogs.unshift(data); // добавить запись
+    }
+  }
 }

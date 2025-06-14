8b21863d3008650679c803ce494b2a208a4fa4e9
Ваше Имя <@email.com>
2025-06-14 12:30:20 +0300

    интро

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss'],
+  styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent  implements OnInit {
+export class IntroComponent implements AfterViewInit {
+  slides = [
+    {
+      image: './assets/sample-images/prikol2.jpg',
+      title: 'Это приложение создано с использованием Ionic 7.',
+      paragraphs: [
+        'Создано командой IonicThemes, чтобы помочь вам начать разработку.',
+        'У нас есть множество обучающих материалов!'
+      ]
+    },
+    {
+      image: './assets/sample-images/prikol.jpg',
+      title: 'Как использовать шаблон?',
+      paragraphs: [
+        'Создавайте качественные и быстрые приложения на Ionic.',
+        'Переиспользуйте и кастомизируйте компоненты.'
+      ]
+    },
+    {
+      image: './assets/sample-images/prikol3.jpg',
+      title: 'Какие компоненты доступны?',
+      paragraphs: [
+        'Вкладки, формы, аутентификация и многое другое.',
+        'Подходит для любых сценариев: еда, недвижимость и т.д.'
+      ]
+    }
+  ];
 
-  constructor() { }
+  swiperRef: any;
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+  this.swiperRef = document.querySelector('swiper-container')?.swiper;
+  new Swiper('.intro-swiper', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+}
+
   
 
+  ,
+      autoplay: {
+        delay: 10000,
+        disableOnInteraction: false
+      },
+    });
+  }
 }

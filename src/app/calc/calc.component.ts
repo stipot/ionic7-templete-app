a8480ffd19c793d15c5423dd490a796dac857cf6
Mak1sss <2315031@mail.ru>
2025-11-01 09:34:27 +0000

    feat: add  symbols from keyboard

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener  } from '@angular/core';
+import { and, or } from 'firebase/firestore';
 
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
   styleUrls: ['./calc.component.scss'],
 })
-export class CalcComponent  implements OnInit {
+export class CalcComponent {
   expression: string = '';
   buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.','+'];
 
@@ -13,6 +14,24 @@ export class CalcComponent  implements OnInit {
     this.expression += value;
   }
 
+@HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const key = event.key;
+
+    // Разрешённые символы
+    if (/^[0-9+\-*/.]$/.test(key)) {
+      this.expression += key;
+    }
+    else if ((key === 'Enter') || (key === '=')) {
+      this.calculate();
+    }
+    else if (key === 'Backspace') {
+      this.expression = this.expression.slice(0, -1);
+    }
+    else if ((key === 'Escape') || (key.toLowerCase() === 'c')) {
+      this.clear();
+    }
+  }
   calculate() {
     try {
       this.expression = eval(this.expression).toString();

3cd63de318110905d2500c43339c814189fc7d2c
nesquikduo <nastya.margorina28@mail.ru>
2026-04-04 18:49:16 +0000

    FirstCommit

---
+++ src/app/wardrobe/wardrobe.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wardrobe',
+  templateUrl: './wardrobe.component.html',
+  styleUrls: ['./wardrobe.component.scss'],
+})
+export class WardrobeComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
